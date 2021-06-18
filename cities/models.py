08f07965f6f7bb00bbd79bb4d1f21ebdd82b3e52
Andrey Smakotin <slamfromm@gmail.com>
2021-06-19 00:51:26 +0300

    some fix

--- cities/models.py
+++ cities/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
