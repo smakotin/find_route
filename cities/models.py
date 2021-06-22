ed1316e83e013c7fa859469f8358a6f4ecc4796f
Andrey Smakotin <slamfromm@gmail.com>
2021-06-22 09:44:04 +0300

    city detail html, return button

--- cities/models.py
+++ cities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class City(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название города')
 
     def __str__(self):
         return self.name
