3c948be4b9b65f7bfcb868100e3359772f463e13
Andrey Smakotin <slamfromm@gmail.com>
2021-08-02 20:19:20 +0300

    Реализовано сохранение маршрута

--- routes/models.py
+++ routes/models.py
@@ -14,7 +14,7 @@ class Route(models.Model):
     trains = models.ManyToManyField('trains.Train', verbose_name='Список поездов')
 
     def __str__(self):
-        return f'Маршрут №{self.name} из города {self.from_city}'
+        return f'Маршрут {self.name} из города {self.from_city}'
 
     class Meta:
         verbose_name = 'Маршрут'
--- travel/settings.py
+++ travel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz^i=$yfr1d=*d18eb_xd%^5-oe-n$)4j@!=t0ydbq-22v_=%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
